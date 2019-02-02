d297210fe8a81480ac1043e60e9ee648dc1bba2d
amandashu <shu.amanda25@gmail.com>
2019-02-01 19:20:03 -0800

    add legend and change sizing

--- clean_data.py
+++ clean_data.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Fri Dec 28 15:43:03 2018
 
 @author: Amanda
 """
-# -*- coding: utf-8 -*-
+
 # imports
 import pandas as pd
 import numpy as np
@@ -14,22 +14,22 @@ from os.path import dirname, join, basename
 
 def create_data():
     parking = pd.read_excel(join(dirname(__file__),'data', 'parking.xlsx'), sheet_name='Sheet1')
-    
+
     # move time to row 2 and make row 2 the column names
     parking.iloc[2][7:] = parking.iloc[1][7:]
-    
+
     # get the data without empty rows/columns
     parking.drop(columns='Unnamed: 5',inplace=True)
     parking_data = parking.iloc[4:,1:]
-    
+
     # get structure/lot column names
     lots = list(parking.iloc[2][1:5])
-    
+
     # get structure/lots data
     common_data = parking_data.iloc[:,0:4].reset_index().iloc[:,1:5]
     common_data.columns = lots
-    
-    xy = {'Hopkins':(600, 1040), 
+
+    xy = {'Hopkins':(600, 1040),
                    'Oster':(715, 435),
                    'Pangea':(425,1060),
                    'Torrey Pines Center North':(450,1530),
@@ -45,16 +45,16 @@ def create_data():
                    'P705':(1540,860),
                    'P782':(1570,560)
                     }
-    
+
     x_values = np.array([])
     y_values = np.array([])
     for x,y in xy.values():
         x_values = np.append(x_values, x)
         y_values = np.append(y_values, y)
-    
+
     common_data['x'] = pd.Series(x_values)
     common_data['y'] = pd.Series(y_values)
-    
+
     # create a array called structure_lot to replace Structure and Lot
     structure_lot = np.array([])
     for i in np.arange(len(common_data)):
@@ -62,52 +62,52 @@ def create_data():
             structure_lot = np.append(structure_lot,common_data.iloc[i,0])
         elif isinstance(common_data.iloc[i,0], float) == True and isinstance(common_data.iloc[i,1], float) == False:
             structure_lot = np.append(structure_lot,common_data.iloc[i,1])
-        else: 
+        else:
              structure_lot = np.append(structure_lot,common_data.iloc[i,0] + ' (' + common_data.iloc[i,1] + ')')
-    
+
     # drop Structure, Lot, and Type columns
     common_data = common_data.iloc[:,3:]
-    
+
     # add new columnes Structure/Lot to dataframe
-    common_data['Structure/Lot'] = structure_lot
-    
+    common_data['Name'] = structure_lot
+
     # reorder columns to Structure/Lot, Longitude Latitude, Total Spaces
     columns_to_reorder = common_data.columns
     reordered = np.array([columns_to_reorder[-1],columns_to_reorder[1], columns_to_reorder[2],columns_to_reorder[0]])
     common_data = common_data[reordered]
-    
+
     # create array of times
     times = np.array(['8AM','10AM','12PM','2PM'])
-    
+
     # create empty dataframe
     mon_parking = pd.DataFrame([])
     tues_parking = pd.DataFrame([])
     wed_parking = pd.DataFrame([])
     thurs_parking = pd.DataFrame([])
     days = [mon_parking, tues_parking, wed_parking, thurs_parking]
-    
+
     # split parking_data into data by each day
     for i in range(4):
         start_index = 4+i*4
         end_index = 8+i*4
         days[i] = pd.DataFrame(parking_data.iloc[:,start_index:end_index].reset_index().iloc[:,1:])
         days[i].columns = times
-    
-    # create dataframes for each day    
+
+    # create dataframes for each day
     mon_parking = common_data.merge(days[0], left_index=True, right_index=True)
     tues_parking = common_data.merge(days[1], left_index=True, right_index=True)
     wed_parking = common_data.merge(days[2], left_index=True, right_index=True)
     thurs_parking = common_data.merge(days[3], left_index=True, right_index=True)
-    
+
     # create list of each of the dataframes
     day_dataframes = [mon_parking, tues_parking, wed_parking, thurs_parking]
-    
+
     # create columns for percentages of available spaces
     for df in day_dataframes:
         for i in np.arange(4,8):
             new_col_name = df.columns[i] + ' % Open Spaces'
             df[new_col_name] = df.iloc[:,i]/df.iloc[:,3]
-            
+
     # add columns for colors by percentage of open spaces
     for df in day_dataframes:
         for i in np.arange(8,12):
@@ -124,10 +124,18 @@ def create_data():
                     colors = np.append(colors,'khaki')
                 else:
                     colors = np.append(colors,'seagreen')
-            df[new_col_name] = colors  
-    
-    # add a column of scaled total spaces for graphing
+            df[new_col_name] = colors
+
+    # add a column of size
     for df in day_dataframes:
-        df['Scaled Total Spaces'] = df['Total Spaces']/14
-        
+        sizes = np.array([])
+        for i in np.arange(16):
+            if df['Total Spaces'][i] < 230:
+                sizes = np.append(sizes,10)
+            elif df['Total Spaces'][i] > 230 and df['Total Spaces'][i] < 460:
+                sizes = np.append(sizes,20)
+            elif df['Total Spaces'][i] > 460 and df['Total Spaces'][i] < 690:
+                sizes = np.append(sizes,30)
+        df['sizes'] = sizes
+
     return mon_parking, tues_parking, wed_parking, thurs_parking, day_dataframes
--- main.py
+++ main.py
@@ -1,38 +1,79 @@
-    
-# bokeh imports
-from bokeh.io import output_file, show, curdoc
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool, Select, RadioButtonGroup
-from bokeh.layouts import column
-from os.path import dirname, join, basename
-from clean_data import create_data
+#note to self:
+#cd to Guardian folder
+# run using bokeh serve parking --show
 
+# bokeh imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, HoverTool, Select
+from bokeh.layouts import column, row
+from clean_data import create_data
+#from os.path import dirname, join, basename
+
+# get the final dataframes
 mon_parking, tues_parking, wed_parking, thurs_parking, day_dataframes = create_data()
 
+# create ColumnDataSource
 data = {
+        'Name': day_dataframes[0]['Name'],
         'x': day_dataframes[0]['x'],
         'y': day_dataframes[0]['y'],
         'colors' : day_dataframes[0]['8AM colors'],
-        'Scaled Total Spaces': day_dataframes[0]['Scaled Total Spaces'],
+        'sizes': day_dataframes[0]['sizes'],
         'Total Spaces': day_dataframes[0]['Total Spaces'],
         '% Open Spaces': day_dataframes[0]['8AM % Open Spaces'],
         'Open Spaces' : day_dataframes[0]['8AM'],
         }
 source = ColumnDataSource(data)
+
+# plot figure
 p = figure(x_range=(0,1700), y_range=(0,1600))
-p.image_url(url=[join(basename(dirname(__file__)), 'static', 'map.png')], x=0, y=0,w=1700,h=1600, anchor='bottom_left')
-p.diamond(x='x',y='y',color='colors', size = 'Scaled Total Spaces',source=source)
+p.image_url(url=['parking/static/map.png'], x=0, y=0,w=1700,h=1600, anchor='bottom_left')
+#p.image_url(url=[join(basename(dirname(__file__)), 'static', 'map.png')], x=0, y=0,w=1700,h=1600, anchor='bottom_left')
+p.circle(x='x',y='y',color='colors', size = 'sizes',source=source,line_color='black')
+
+# legend box
+p.rect(x=1225,y=1400,width=900,height=350,fill_color='white',line_color='black')
+
+# legend texts
+xtext_pos = [905]*5 + [1330]*3
+ytext_pos = list(range(1450,1200,-50)) + [1450,1380,1300]
+legtext = ["<5%","5%-25%","25%-50%","50%-75%",">75%"] + ["<230 spots","230-460 spots", ">460 spots"]
+p.text(x=xtext_pos,y=ytext_pos,text=legtext)
+
+# legend titles
+xtitle_pos = [810,1230]
+ytitle_pos = [1515]*2
+legtext = ["% Open Spots"] + ["Parking/Lot Size"]
+p.text(x=xtitle_pos,y=ytitle_pos,text=legtext,text_font_style='bold')
+
+# color circles
+c1_xpos = [850]*5
+c1_ypos = list(range(1475,1250,-50))
+c1_col = ['darkred','firebrick','orange','khaki','seagreen']
+p.circle(x=c1_xpos,y=c1_ypos,color=c1_col,size = 10,line_color='black')
+
+# size circle
+c2_xpos = [1270]*3
+c2_ypos = [1480,1420,1330]
+c2_size = [10,20,30]
+p.circle(x=c2_xpos, y=c2_ypos, size=c2_size, fill_color='white', line_color='black')
+
+# add Hovertool
 hover = HoverTool(
     tooltips = [
+                ('Structure/Lot','@Name'),
                 ('Total Spaces', '@{Total Spaces}'),
                 ('% of Open Spaces','@{% Open Spaces}'),
                 ('# of Open Spaces','@{Open Spaces}')
 ])
 p.add_tools(hover)
 
-
+# create buttons to select day and time
 day_select = Select(options = ['Monday','Tuesday', 'Wednesday','Thursday'], value='Monday', title='Select Day')
 time_select = Select(options= ['8AM','10AM','12PM','2PM'], value='8AM',title='Select Time')
+
+# define callback for the day and time buttons
 def update_plot(attr, old, new):
     N = day_select.value
     t = time_select.value
@@ -46,47 +87,52 @@ def update_plot(attr, old, new):
         i=3
     if t == '8AM':
         new_data = {
+                'Name': day_dataframes[0]['Name'],
                 'x': day_dataframes[i]['x'],
                 'y': day_dataframes[i]['y'],
                 'colors' : day_dataframes[i]['8AM colors'],
-                'Scaled Total Spaces': day_dataframes[i]['Scaled Total Spaces'],
+                'sizes': day_dataframes[i]['sizes'],
                 'Total Spaces': day_dataframes[i]['Total Spaces'],
                 '% Open Spaces': day_dataframes[i]['8AM % Open Spaces'],
                 'Open Spaces': day_dataframes[i]['8AM'],
         }
     elif t == '10AM':
         new_data = {
+                'Name': day_dataframes[0]['Name'],
                 'x': day_dataframes[i]['x'],
                 'y': day_dataframes[i]['y'],
                 'colors' : day_dataframes[i]['10AM colors'],
-                'Scaled Total Spaces': day_dataframes[i]['Scaled Total Spaces'],
+                'sizes': day_dataframes[i]['sizes'],
                 'Total Spaces': day_dataframes[i]['Total Spaces'],
                 '% Open Spaces': day_dataframes[i]['10AM % Open Spaces'],
                 'Open Spaces': day_dataframes[i]['10AM'],
         }
     elif t == '12PM':
         new_data = {
+                'Name': day_dataframes[0]['Name'],
                 'x': day_dataframes[i]['x'],
                 'y': day_dataframes[i]['y'],
                 'colors' : day_dataframes[i]['12PM colors'],
-                'Scaled Total Spaces': day_dataframes[i]['Scaled Total Spaces'],
+                'sizes': day_dataframes[i]['sizes'],
                 'Total Spaces': day_dataframes[i]['Total Spaces'],
                 '% Open Spaces': day_dataframes[i]['12PM % Open Spaces'],
                 'Open Spaces': day_dataframes[i]['12PM'],
         }
     elif t == '2PM':
         new_data = {
+                'Name': day_dataframes[0]['Name'],
                 'x': day_dataframes[i]['x'],
                 'y': day_dataframes[i]['y'],
                 'colors' : day_dataframes[i]['2PM colors'],
-                'Scaled Total Spaces': day_dataframes[i]['Scaled Total Spaces'],
+                'sizes': day_dataframes[i]['sizes'],
                 'Total Spaces': day_dataframes[i]['Total Spaces'],
                 '% Open Spaces': day_dataframes[i]['2PM % Open Spaces'],
                 'Open Spaces': day_dataframes[i]['2PM'],
-        }       
+        }
     source.data = new_data
 
 day_select.on_change('value',update_plot)
 time_select.on_change('value', update_plot)
-layout = column(day_select, time_select, p)
+
+layout = column(row(day_select, time_select), p)
 curdoc().add_root(layout)
